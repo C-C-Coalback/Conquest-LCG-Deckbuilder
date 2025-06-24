df7f647fea18dc3ab61201b04b2196843f4b5ed5
C-C-Coalback <argarfield18@gmail.com>
2025-06-24 12:38:00 +0200

    Created pages for cards. Pages include details such as faction, text, cost, stats if relevant, bans and errata.

--- conquestdb/cards/views.py
+++ conquestdb/cards/views.py
@@ -7,13 +7,25 @@ from django.http import JsonResponse
 card_array = Initfunctions.init_player_cards()
 planet_array = Initfunctions.init_planet_cards()
 apoka_errata_array = Initfunctions.init_apoka_errata_cards()
-
+images_dict = {}
+cards_dict = {}
+apoka_errata_dict = {}
+banned_cards = ["Bonesinger Choir", "Squiggoth Brute", "Corrupted Teleportarium", "Gun Drones", "Archon's Palace",
+                "Land Speeder Vengeance", "Sowing Chaos", "Smasha Gun Battery", "The Prince's Might",
+                "Purveyor of Hubris", "Doom", "Exterminatus", "Mind Shackle Scarab",
+                "Crypt of Saint Camila", "Warp Storm"]
+for key in range(len(card_array)):
+    cards_dict[card_array[key].name] = card_array[key]
+    images_dict[card_array[key].image_name] = card_array[key]
+for key in range(len(apoka_errata_array)):
+    apoka_errata_dict[apoka_errata_array[key].image_name] = apoka_errata_array[key]
 
 df = pd.DataFrame([x.as_dict() for x in card_array])
 
 
 def index(request):
     return render(request, 'cards/index.html')
+
 
 # View to handle the Ajax request
 def ajax_view(request):
@@ -38,4 +50,38 @@ def ajax_view(request):
 
 
 def card_data(request, card_name):
-    return render(request, "cards/card_data.html", {"card_name": card_name})
+    if card_name not in images_dict:
+        return render(request, 'cards/index.html')
+    card = images_dict[card_name]
+    original_card_name = card.name
+    image_name = card.image_name
+    print(original_card_name)
+    text = card.text
+    card_type = card.card_type
+    cost = str(card.cost)
+    command = -1
+    attack = -1
+    health = -1
+    is_unit = "False"
+    loyalty = card.loyalty
+    faction = card.faction
+    traits = card.traits
+    if card.is_unit:
+        if card_type != "Warlord":
+            card_type = card_type + " Unit"
+        is_unit = "True"
+        command = str(card.command)
+        attack = str(card.attack)
+        health = str(card.health)
+    errata_text = "No Errata"
+    ban_text = "No Bans"
+    if card_name in apoka_errata_dict:
+        errata_text = "There is Errata"
+    if original_card_name in banned_cards:
+        ban_text = "Banned"
+    return render(request, "cards/card_data.html", {"card_name": original_card_name, "image_name": image_name,
+                                                    "text": text, "card_type": card_type, "cost": cost,
+                                                    "command": command, "attack": attack, "health": health,
+                                                    "is_unit": is_unit, "loyalty": loyalty, "faction": faction,
+                                                    "traits": traits, "ban_text": ban_text,
+                                                    "errata_text": errata_text})
