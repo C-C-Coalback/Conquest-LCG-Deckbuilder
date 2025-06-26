0899c03499e8f1401933eb1dc1546075851c8b6d
C-C-Coalback <argarfield18@gmail.com>
2025-06-26 12:51:27 +0200

    Saving decks supported.

--- conquestdb/decks/views.py
+++ conquestdb/decks/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from conquestdb.cardscode import Initfunctions
 from conquestdb.cardscode import FindCard
+import os
+import copy
 
 
 card_array = Initfunctions.init_player_cards()
@@ -22,6 +24,155 @@ for key in range(len(apoka_errata_array)):
 
 
 alignment_wheel = ["Astra Militarum", "Space Marines", "Tau", "Eldar", "Dark Eldar", "Chaos", "Orks"]
+
+
+def clean_sent_deck(deck_message):
+    print("Code to test if deck is ok")
+    deck_sections = deck_message.split(sep="------------------"
+                                           "----------------------------------------------------")
+    print(deck_sections)
+    individual_parts = []
+    for i in range(len(deck_sections)):
+        individual_parts += deck_sections[i].split(sep="\n")
+    individual_parts = [x for x in individual_parts if x]
+    return individual_parts
+
+
+def second_part_deck_validation(deck):
+    global card_array
+    global cards_dict
+    print("Size should be fine")
+    name = deck[0]
+    res = name != '' and all(c.isalnum() or c.isspace() for c in name)
+    if len(name) > 27:
+        return "Name too long"
+    elif not res:
+        return "Name contains non-alphanumeric characters"
+    warlord_card = FindCard.find_card(deck[1], card_array, cards_dict)
+    if warlord_card.get_card_type() != "Warlord":
+        print("Card in Warlord position is not a warlord")
+        return "Card in Warlord position is not a warlord"
+    remaining_signature_squad = copy.deepcopy(warlord_card.get_signature_squad())
+    print("Warlord name: ", warlord_card.get_name())
+    print("Remaining signature squad list: ", remaining_signature_squad)
+    factions = deck[2].split(sep=" (")
+    if len(factions) == 2:
+        factions[1] = factions[1][:-1]
+    print(factions)
+    warlord_matches = True
+    if factions[0] != warlord_card.get_faction():
+        print("Faction chosen does not match the warlord")
+        return "Warlord does not match main faction"
+    if len(factions) == 1 and warlord_matches:
+        factions.append("None")
+        return deck_validation(deck, remaining_signature_squad, factions, warlord_card.get_name())
+    if len(factions) == 2 and warlord_matches:
+        if factions[0] == factions[1]:
+            print("Main faction and ally faction can not be the same")
+            return "Main faction and ally faction can not be the same"
+        alignment_wheel = ["Astra Militarum", "Space Marines", "Tau", "Eldar",
+                           "Dark Eldar", "Chaos", "Orks"]
+        position_main_faction = -1
+        for i in range(len(alignment_wheel)):
+            if alignment_wheel[i] == factions[0]:
+                position_main_faction = i
+        if position_main_faction != -1:
+            ally_pos_1 = (position_main_faction + 1) % 7
+            ally_pos_2 = (position_main_faction - 1) % 7
+            if factions[1] == alignment_wheel[ally_pos_1] \
+                    or factions[1] == alignment_wheel[ally_pos_2]:
+                return deck_validation(deck, remaining_signature_squad, factions)
+            elif factions[1] == "Astra Militarum" and warlord_card.get_name() == "Commander Starblaze":
+                return deck_validation(deck, remaining_signature_squad, factions, "Commander Starblaze")
+        return "Issue with faction matching."
+    return "Unknown issue"
+
+
+def deck_validation(deck, remaining_signature_squad, factions, warlord=""):
+    global card_array
+    global cards_dict
+    print("Can continue")
+    current_index = 4
+    while deck[current_index] != "Army":
+        if deck[current_index] in remaining_signature_squad:
+            print("Found a match")
+            try:
+                remaining_signature_squad.remove(deck[current_index])
+            except ValueError:
+                print("How?")
+            print(remaining_signature_squad)
+        else:
+            print("No match")
+            return "Unexpected Card in Signature Squad: " + deck[current_index]
+        current_index += 1
+    if len(remaining_signature_squad) > 0:
+        return "Missing something from signature squad"
+    synapse_needed = False
+    has_synapse = False
+    if factions[0] == "Tyranids":
+        synapse_needed = True
+    current_index += 1
+    card_count = 0
+    skippers = ["Support", "Attachment", "Event", "Synapse"]
+    while deck[current_index] != "Planet" and current_index < len(deck):
+        if len(deck[current_index]) > 3:
+            current_name = deck[current_index][3:]
+            current_amount = deck[current_index][0]
+            try:
+                card_count += int(current_amount)
+                if int(current_amount) > 3:
+                    print("Too many copies")
+                    return "Too many copies: " + current_name
+            except ValueError:
+                return "Number missing"
+            card_result = FindCard.find_card(current_name, card_array, cards_dict)
+            if card_result.get_name() != current_name:
+                print("Card not found in database", current_name)
+                return "Card not found in database: " + current_name
+            if card_result.get_loyalty() == "Signature":
+                print("Signature card found")
+                return "Signature card found: " + current_name
+            if card_result.get_card_type() == "Synapse":
+                if synapse_needed:
+                    if has_synapse:
+                        return "Too many Synapse units given"
+                    else:
+                        if current_amount != "1":
+                            return "Wrong number for synapse unit"
+                        has_synapse = True
+                else:
+                    return "Synapse units not allowed in this deck"
+            faction_check_passed = False
+            if card_result.get_faction() == factions[0]:
+                faction_check_passed = True
+            elif card_result.get_faction() == factions[1] and card_result.get_loyalty() == "Common":
+                faction_check_passed = True
+            elif factions[0] == "Necrons" and card_result.get_faction() != "Tyranids" and \
+                    card_result.get_loyalty() == "Common" and card_result.get_card_type() == "Army":
+                faction_check_passed = True
+            elif card_result.get_faction() == "Neutral":
+                if factions[0] == "Tyranids" and card_result.get_card_type() == "Army":
+                    return "Tyranids may not have Neutral Army Units in their deck"
+                faction_check_passed = True
+            elif warlord == "Gorzod":
+                if card_result.get_faction() == "Astra Militarum" or card_result.get_faction() == "Space Marines":
+                    if card_result.get_card_type() == "Army" and card_result.get_loyalty() == "Common":
+                        if card_result.check_for_a_trait("Vehicle"):
+                            faction_check_passed = True
+            if not faction_check_passed:
+                print("Faction check not passed", factions[0], factions[1], card_result.get_faction())
+                return "Faction check not passed (Main, Ally, Card): "\
+                       + factions[0] + factions[1] + card_result.get_faction()
+        current_index += 1
+        while deck[current_index] in skippers:
+            current_index += 1
+    if synapse_needed and not has_synapse:
+        return "No Synapse Unit Given"
+    if card_count < 42:
+        print("Too few cards")
+        return "Too few cards: " + str(card_count)
+    print("No issues")
+    return "SUCCESS"
 
 
 def decks(request):
@@ -45,8 +196,44 @@ def ajax_view(request):
         flag = request.POST.get('flag')
         if flag == "SENDDECK":
             text = request.POST.get('deck_text')
-            print(text)
-            return JsonResponse({'message': 'Saving'})
+            username = request.POST.get('username')
+            description = request.POST.get('description')
+            message_to_send = ""
+            text = text.replace("\"Subject: &Omega;-X62113\"", "")
+            text = text.replace("idden Base", "'idden Base")
+            text = text.replace("\"", "")
+            deck = clean_sent_deck(text)
+            print(deck)
+            print("got here")
+            deck_name = deck[0]
+            if len(deck) > 5:
+                if "{AUTOMAIN}" in deck[2]:
+                    warlord = FindCard.find_card(deck[1], card_array, cards_dict)
+                    deck[2] = deck[2].replace("{AUTOMAIN}", warlord.get_faction())
+                    text = text.replace("{AUTOMAIN}", warlord.get_faction())
+                message_to_send = second_part_deck_validation(deck)
+            print("got here 2")
+            if message_to_send == "SUCCESS":
+                print("Need to save deck")
+                print(os.getcwd())
+                directory = os.getcwd()
+                target_directory = directory + "/decks/deckstorage/" + username + "/"
+                os.makedirs(target_directory, exist_ok=True)
+                target_directory = directory + "/decks/deckstorage/" + username + "/" + deck_name
+                if not os.path.exists(target_directory):
+                    print("Path does not exist")
+                    os.mkdir(target_directory)
+                else:
+                    print("path exists")
+                    return JsonResponse({'message': 'deck with that name already exists'})
+                with open(target_directory + "/content", "w") as file:
+                    file.write(text)
+                with open(target_directory + "/desc", "w") as file:
+                    file.write(description)
+                return JsonResponse({'message': 'deck ok'})
+            message_to_send = "Feedback/" + message_to_send
+            message = message_to_send
+            return JsonResponse({'message': 'deck not ok'})
         if flag == "SETALLY":
             ally_name = request.POST.get('ally_faction')
             warlord_name = request.POST.get('warlord_name')
@@ -108,6 +295,8 @@ def ajax_view(request):
             elif main_faction == "Tyranids":
                 if card.get_faction() == "Tyranids" or (card.get_faction() == "Neutral" and card_type != "Army"):
                     ally_ok = True
+            elif card.get_faction() == "Neutral":
+                ally_ok = True
             elif warlord_name == "Commander Starblaze":
                 if ally == "Astra Militarum" and card.get_faction() == "Astra Militarum" and\
                         card.get_loyalty() == "Common":
