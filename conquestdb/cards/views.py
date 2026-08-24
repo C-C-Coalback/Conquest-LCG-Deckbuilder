68006bc518920684f935a9f0fe7794f2ec696523
C-C-Coalback <argarfield18@gmail.com>
2026-08-24 21:27:40 +0200

    spam prevention

--- conquestdb/cards/views.py
+++ conquestdb/cards/views.py
@@ -1070,15 +1070,16 @@ def card_data(request, card_name):
             username = request.POST.get('username')
             if not username:
                 username = "Anonymous"
-            if username != "Eric Jones":
-                comment = request.POST.get('comment')
-                time = str(datetime.datetime.now())
-                os.makedirs(target_directory, exist_ok=True)
-                file_id = len(
-                    [name for name in os.listdir(target_directory) if os.path.isfile(target_directory + "/" + name)])
-                name_file = str(file_id) + ".txt"
-                with open(target_directory + name_file, 'w') as file:
-                    file.write(username + "\n" + time + "\n" + comment)
+            comment = request.POST.get('comment')
+            if card_name == "Bonesinger_Choir" and "hello conquestdb" in comment:
+                return redirect("/")
+            time = str(datetime.datetime.now())
+            os.makedirs(target_directory, exist_ok=True)
+            file_id = len(
+                [name for name in os.listdir(target_directory) if os.path.isfile(target_directory + "/" + name)])
+            name_file = str(file_id) + ".txt"
+            with open(target_directory + name_file, 'w') as file:
+                file.write(username + "\n" + time + "\n" + comment)
         elif flag == "DELETE":
             # username = request.POST.get('username')
             id_c = request.POST.get('idcomment')
